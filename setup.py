b501309acf620bd772e74c0cbf9ed6a50ad2b9c6
Vipul Patil <vipulpatil7057@gmail.com>
2023-11-11 11:55:13 +0100

    Modified setup.py

--- setup.py
+++ setup.py
@@ -4,9 +4,4 @@ setup(
     name='math_quiz',
     version='0.1',
     packages=find_packages(),
-    entry_points={
-        'console_scripts': [
-            'math_quiz = math_quiz.math_quiz:main',
-        ],
-    },
 )
