8bb7316aa38e36b496c42a69360222dcdbb1f9aa
Vipul Patil <vipulpatil7057@gmail.com>
2023-11-11 12:19:09 +0100

    Modified setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,15 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='math_quiz',
+    name='math-quiz',
     version='0.1',
     packages=find_packages(),
+    install_requires=[
+        # Specify any dependencies your project needs
+    ],
+    entry_points={
+        'console_scripts': [
+            'math_quiz = math_quiz.math_quiz:main',
+        ],
+    },
 )
